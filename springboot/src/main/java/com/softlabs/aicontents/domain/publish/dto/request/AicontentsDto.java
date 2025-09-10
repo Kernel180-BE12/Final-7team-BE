f798f03ea52a5272bc709eb79acbf6e68d3904fc
dongjae <dj8@naver.com>
2025-09-10 19:09:24 +0900

    feat: 기본 구조 êµ추가

---
+++ springboot/src/main/java/com/softlabs/aicontents/domain/publish/dto/request/AicontentsDto.java
@@ -0,0 +1,11 @@
+package com.softlabs.aicontents.domain.publish.dto.request;
+
+import lombok.Data;
+
+@Data
+public class ContentsDto {
+    private String Title;
+    private String Description;
+    
+
+}
---
+++ springboot/src/main/java/com/softlabs/aicontents/domain/publish/dto/request/PublishReqDto.java
@@ -0,0 +1,4 @@
+package com.softlabs.aicontents.domain.publish.dto.request;
+
+public class PubishReqDto {
+}
