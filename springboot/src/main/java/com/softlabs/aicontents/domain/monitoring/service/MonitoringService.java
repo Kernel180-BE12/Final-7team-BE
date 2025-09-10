e73e54fb118f0202cdd3c404678e0ac3e3698f0c
leejh <leejxhxxx0159@gmail.com>
2025-09-10 19:06:24 +0900

    chore: remove unused code

--- springboot/src/main/java/com/softlabs/aicontents/domain/monitoring/service/MonitoringService.java
+++
@@ -1,7 +0,0 @@
-package com.softlabs.aicontents.domain.monitoring.service;
-
-import com.softlabs.aicontents.domain.monitoring.dto.response.MonitoringStatsResponseDTO;
-
-public interface MonitoringService {
-    MonitoringStatsResponseDTO getStats();
-}
