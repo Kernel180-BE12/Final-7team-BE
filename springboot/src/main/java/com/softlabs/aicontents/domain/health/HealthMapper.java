3db98be0afcbd1dc4a5293f06b9a93401e9a9fc2
leejh <leejxhxxx0159@gmail.com>
2025-09-11 16:24:22 +0900

    docs: /api/health 주석 처리

--- springboot/src/main/java/com/softlabs/aicontents/domain/health/HealthMapper.java
+++ springboot/src/main/java/com/softlabs/aicontents/domain/health/HealthMapper.java
@@ -1,8 +1,8 @@
-package com.softlabs.aicontents.domain.health;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface HealthMapper {
-  String getSysdate();
-}
+//package com.softlabs.aicontents.domain.health;
+//
+//import org.apache.ibatis.annotations.Mapper;
+//
+//@Mapper
+//public interface HealthMapper {
+//  String getSysdate();
+//}
