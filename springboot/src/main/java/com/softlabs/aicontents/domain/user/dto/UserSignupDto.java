dc5725a05d7a007fd352a502d27eee3685bb98b1
JungSoonIn <tnsdls1966@naver.com>
2025-09-23 15:35:27 +0900

    chore: spotlessApply

--- springboot/src/main/java/com/softlabs/aicontents/domain/user/dto/UserSignupDto.java
+++ springboot/src/main/java/com/softlabs/aicontents/domain/user/dto/UserSignupDto.java
@@ -6,16 +6,16 @@ import lombok.Data;
 @Data
 public class UserSignupDto {
 
-    @NotBlank(message = "{common.login-id.not-blank}")
-    @Size(min = 4, max = 20, message = "{common.login-id.size}")
-    private String loginId;
+  @NotBlank(message = "{common.login-id.not-blank}")
+  @Size(min = 4, max = 20, message = "{common.login-id.size}")
+  private String loginId;
 
-    @Email(message = "{common.email.invalid}")
-    @NotBlank(message = "{common.email.not-blank}")
-    private String email;
+  @Email(message = "{common.email.invalid}")
+  @NotBlank(message = "{common.email.not-blank}")
+  private String email;
 
-    @NotBlank(message = "{common.password.not-blank}")
-    @Size(min = 8, max = 20, message = "{common.password.size}")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$", message = "{common.password.pattern}")
-    private String password;
+  @NotBlank(message = "{common.password.not-blank}")
+  @Size(min = 8, max = 20, message = "{common.password.size}")
+  @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$", message = "{common.password.pattern}")
+  private String password;
 }
