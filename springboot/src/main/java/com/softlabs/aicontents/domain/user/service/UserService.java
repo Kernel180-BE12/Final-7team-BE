66a8201d933da78889752f3c0cd7aeb3065a1500
JungSoonIn <tnsdls1966@naver.com>
2025-09-23 15:45:25 +0900

    feat: 비밀번호 해싱이 적용된 UserService 구현 - BCrypt 해싱이 적용된 사용자 생성 로직 추가 - 비밀번호 검증 기능 추가

---
+++ springboot/src/main/java/com/softlabs/aicontents/domain/user/service/UserService.java
@@ -0,0 +1,36 @@
+package com.softlabs.aicontents.domain.user.service;
+
+import com.softlabs.aicontents.domain.user.dto.UserSignupDto;
+import com.softlabs.aicontents.domain.user.vo.User;
+import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+  private final PasswordService passwordService;
+
+  @Autowired
+  public UserService(PasswordService passwordService) {
+    this.passwordService = passwordService;
+  }
+
+  public User createUser(UserSignupDto signupDto) {
+    String hashedPassword = passwordService.hashPassword(signupDto.getPassword());
+
+    LocalDateTime now = LocalDateTime.now();
+    return new User(
+        null,
+        signupDto.getLoginId(),
+        signupDto.getEmail(),
+        hashedPassword,
+        now,
+        now,
+        null);
+  }
+
+  public boolean validatePassword(String plainPassword, String hashedPassword) {
+    return passwordService.matches(plainPassword, hashedPassword);
+  }
+}
