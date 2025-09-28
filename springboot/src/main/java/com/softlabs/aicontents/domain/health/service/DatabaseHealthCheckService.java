a20cdc81f7d8d2cdfd9f339f6f1768b147687a6e
leejh <leejxhxxx0159@gmail.com>
2025-09-28 20:24:33 +0900

    style: spotless로 코드 포맷팅 적용

--- springboot/src/main/java/com/softlabs/aicontents/domain/health/service/DatabaseHealthCheckService.java
+++ springboot/src/main/java/com/softlabs/aicontents/domain/health/service/DatabaseHealthCheckService.java
@@ -2,21 +2,20 @@ package com.softlabs.aicontents.domain.health.service;
 
 import com.softlabs.aicontents.domain.health.mapper.HealthCheckMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor // final 필드 생성자 자동 생성
 public class DatabaseHealthCheckService {
-//  private final JdbcTemplate jdbcTemplate; // DB 접근을 위한 JdbcTemplate
+  //  private final JdbcTemplate jdbcTemplate; // DB 접근을 위한 JdbcTemplate
   private final HealthCheckMapper healthCheckMapper;
 
   public boolean isUp() {
     try {
-//      jdbcTemplate.queryForObject("select 1 from dual", Integer.class);
-      String status=healthCheckMapper.selectDbStatus();
+      //      jdbcTemplate.queryForObject("select 1 from dual", Integer.class);
+      String status = healthCheckMapper.selectDbStatus();
 
-      return status!=null;
+      return status != null;
     } catch (Exception e) {
       e.printStackTrace();
       return false;
